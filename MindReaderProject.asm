071892ee013f6515e2e456dceb169607d00bf806
bbalajiarjun <89600254+bbalajiarjun@users.noreply.github.com>
2021-11-08 17:12:17 -0600

    Add files via upload

---
+++ MindReaderProject.asm
@@ -0,0 +1,42 @@
+.data
+cardOne: .asciiz "CARD ONE";
+side1numberRow1: .asciiz " 1  3  5  7  9 11 13 15"
+side1numberRow2: .asciiz "17 19 21 23 25 27 29 31"
+side1numberRow3: .asciiz "33 35 37 39 41 43 45 47"
+side1numberRow4: .asciiz "49 51 53 55 57 59 61 63"
+cardTwo: .asciiz "CARD TWO";
+side2numberRow1: .asciiz " 2  3  6  7 10 11 14 15"
+side2numberRow2: .asciiz "18 19 22 23 26 27 30 31"
+side2numberRow3: .asciiz "34 35 38 39 42 43 46 47"
+side2numberRow4: .asciiz "50 51 54 55 58 59 62 63"
+cardThree: .asciiz "CARD THREE";
+side3numberRow1: .asciiz " 4  5  6  7 12 13 14 15"
+side3numberRow2: .asciiz "20 21 22 23 28 29 30 31"
+side3numberRow3: .asciiz "36 37 38 39 44 45 46 47"
+side3numberRow4: .asciiz "52 53 54 55 60 61 62 63"
+cardFour: .asciiz "CARD FOUR";
+side4numberRow1: .asciiz " 8  9 10 11 12 13 14 15"
+side4numberRow2: .asciiz "24 25 26 27 28 29 30 31"
+side4numberRow3: .asciiz "40 41 42 43 44 45 46 47"
+side4numberRow4: .asciiz "56 57 58 59 68 61 62 63"
+cardFive: .asciiz "CARD FIVE";
+side5numberRow1: .asciiz "16 17 18 19 20 21 22 23"
+side5numberRow2: .asciiz "24 25 26 27 28 29 30 31"
+side5numberRow3: .asciiz "48 49 50 51 52 53 54 55"
+side5numberRow4: .asciiz "56 57 58 59 60 61 62 63"
+cardSix: .asciiz "CARD SIX";
+side6numberRow1: .asciiz "32 33 34 35 36 37 38 39"
+side6numberRow2: .asciiz "40 41 42 43 44 45 46 47"
+side6numberRow3: .asciiz "48 49 50 51 52 53 54 55"
+side6numberRow4: .asciiz "56 57 58 59 68 61 62 63"
+
+
+.text
+
+Display:
+	
+	
+	
+	
+	
+	
